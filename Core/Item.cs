8be6877df33b654c4e07831145b89bccb743f353
KolibSoft <kolibsoftware@gmail.com>
2023-08-11 15:59:43 -0600

    Item base class added

--- Core/Item.cs
+++ Core/Item.cs
@@ -2,8 +2,12 @@ using KolibSoft.Catalogue.Core.Abstractions;
 
 namespace KolibSoft.Catalogue.Core;
 
-public class Item : IItem
+public class Item : IItem, IValidatable
 {
-    public Guid Id { get; set; }
-    public DateTime UpdatedAt { get; set; }
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public virtual bool Validate(ICollection<string>? errors = default) => true;
+
 }
