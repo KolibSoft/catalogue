205bc35842448b511c4857d61d82c9c659792c06
KolibSoft <kolibsoftware@gmail.com>
2023-10-06 15:43:28 -0600

    New Change handling implementation

--- Client.Example/Program.cs
+++ Client.Example/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System.Text.Json;
 using KolibSoft.Catalogue.Client.Example;
 using KolibSoft.Catalogue.Core;
-using KolibSoft.Catalogue.Core.Abstractions;
 using KolibSoft.Catalogue.Core.Catalogues;
-using KolibSoft.Catalogue.Core.Utils;
 
 var httpClient = new HttpClient();
 var uri = "https://localhost:7072";
 // var uri = "http://localhost:5033";
 var start = DateTime.UtcNow;
 
-var json = File.Exists("changes.json") ? await File.ReadAllTextAsync("changes.json") : "{}";
-var changes = JsonSerializer.Deserialize<IDictionary<Guid, string[]>>(json)!;
+var json = File.Exists("changes.json") ? await File.ReadAllTextAsync("changes.json") : "[]";
+var changes = JsonSerializer.Deserialize<IList<Change>>(json)!;
 
 var connector = new ServiceCatalogue<SettingsModel, object>(
     new DatabaseCatalogue<SettingsModel, object>(new SettingsContext()),
